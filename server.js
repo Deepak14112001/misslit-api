84fa6751a701f072ec94ab1404d3572e69c8d587
deepak <dkparida26@gmail.com>
2026-03-09 03:53:36 +0530

    fix render port

--- server.js
+++ server.js
@@ -111,5 +111,5 @@ app.get("/participants", async (req,res)=>{
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+  console.log(`Server running on port ${PORT}`);
 });
