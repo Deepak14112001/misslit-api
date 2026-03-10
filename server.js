af776f29fb1672f33412f0d6324fd554dbe8a0f1
deepak <dkparida26@gmail.com>
2026-03-10 20:34:37 +0530

    changes

--- server.js
+++ server.js
@@ -8,12 +8,22 @@ const fs = require("fs");
 
 const app = express();
 
+const cors = require("cors");
+
+
+
 /* PORT */
 const PORT = process.env.PORT || 3000;
 
 /* Middleware */
 app.use(cors());
 app.use(express.json());
+
+app.use(cors({
+  origin: "*",
+  methods: ["GET","POST","PUT","DELETE"],
+  allowedHeaders: ["Content-Type"]
+}));
 
 /* Create uploads folder if not exists */
 if (!fs.existsSync("uploads")) {
